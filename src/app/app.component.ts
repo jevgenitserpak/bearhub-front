fc55449bb042cf051a0f21a138b3c473ec491288
Jevgeni <jevgeni.tsherpak@gmail.com>
2020-04-02 00:05:03 +0300

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bearhub-front';
+
+  courses: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.courses = this.http
+      .get('/test/hello', { responseType: 'text' }).toPromise().then(res => this.title = res);
+    // console.log(this.courses);
+  }
 }
+
+
